d3cc7c229f741316f533fc63a9af386f8037f0ec
Rick Xu <xuyy04@gmail.com>
2024-11-10 20:02:13 -0800

    Imported Font & More CSS

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,11 @@ function App() {
 
   return (
     <>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+        <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet"></link>
+      </head>
       <Navbar></Navbar>
       <div id="home">
         <div id="pantryAppContainer">
