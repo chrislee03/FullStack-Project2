c56ace01a3ad566a6a34c1a8c7978318e888974a
chrislee03 <chrislee03@berkeley.edu>
2024-11-11 16:39:24 -0800

    router bugging out need to fix

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,11 +1,7 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Collection from './CollectionApp.jsx';
-import HomeApp from './HomeApp.jsx';
+import NavBar from '../components/NavBar.jsx';
 
 
 function App() {
-
   return (
     <>
       <head>
@@ -13,19 +9,7 @@ function App() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap" rel="stylesheet"></link>
       </head>
-      <Router>
-        <nav> 
-          <div> My Pantry </div> 
-          <ul className="navBarButtons">
-            <li> <Link to="/Home"> Home </Link> </li>
-            <li> <Link to="/Collection"> Collection </Link> </li> 
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/Home" element={<HomeApp/>}/>
-          <Route path='/Collection' element={<Collection/>}/>
-        </Routes>
-      </Router>
+      <NavBar/>
     </>
   )
 }
--- src/Routes/CollectionApp.jsx
+++ src/Routes/CollectionApp.jsx
@@ -1,6 +1,12 @@
+import savedRecipes from '../components/savedRecipes';
+import NavBar from '../components/NavBar.jsx';
+
 const Collection = () => {
-   return (
-    <div> COLLECTION PAGE </div>
+    
+    return (
+        <>
+        <NavBar/>
+        </>
    )
 }
 
